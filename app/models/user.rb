66e478101c060faad130caae659814c281b332f8
maskingu <kengomei@outlook.com>
2020-09-10 18:42:01 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,19 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :email, uniqueness: { case_sensitive: true }, presence: true, format: { with: /@+/, message: '@を使用してください' }
+  EMAIL_REGEX = /@+/.freeze
+  validates :email, uniqueness: { case_sensitive: true }, presence: true, format: { with: EMAIL_REGEX, message: '@を使用してください' }
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' } do
   end
 
-  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  F_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  validates :first_name, :last_name, presence: true, format: { with: F_NAME_REGEX, message: '全角文字を使用してください' } do
   end
 
-  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
+  FK_NAME_REGEX = /\A[ァ-ン]+\z/.freeze
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: FK_NAME_REGEX, message: '全角カナを使用してください' } do
   end
 
   validates :birthday, presence: true
