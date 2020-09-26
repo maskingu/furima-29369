ec1171b98bf15c3a3128719f6adcc91ee4da3fce
maskingu <kengomei@outlook.com>
2020-09-26 19:11:29 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'Capistrano (3.14.1)'
+lock 'capistrano (3.14.1)'
 
 # Capistranoのログの表示に利用する
 set :application, 'furima-29369'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:maskingu/furima-29369.git'
+set :repo_url,  'git@github.com:/maskingu/furima-29369.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
